8be1e93ff225c3c3e401a5708b2c923ff6084740
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-08-13 15:47:44 -0600

    Trying to get the data to render with the addItem button

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,43 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer } from 'react';
 import './App.css';
-
+//components
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm'
 
-const todoData = [
-  {
-    task: 'Organize Garage',
-    id: 1528817077286,
-    completed: false
-  },
-  {
-    task: 'Bake Cookies',
-    id: 1528817084358,
-    completed: false
-  },
-  {
-    task: 'Mow Lawn',
-    id: 1528817086458,
-    completed: false
-  },
-  {
-    task: 'Clean Desk',
-    id: 1528817086439,
-    completed: false
-  },
-  {
-    task: 'Water Plants',
-    id: 1528817086440,
-    completed: false
-  },
-];
+//Reducer
+import { initialState, todoReducer } from './reducers/todoReducer';
+
+// const todoData = [
+//   {
+//     task: 'Organize Garage',
+//     id: 1528817077286,
+//     completed: false
+//   },
+//   {
+//     task: 'Bake Cookies',
+//     id: 1528817084358,
+//     completed: false
+//   }
+//   // {
+//   //   task: 'Mow Lawn',
+//   //   id: 1528817086458,
+//   //   completed: false
+//   // },
+//   // {
+//   //   task: 'Clean Desk',
+//   //   id: 1528817086439,
+//   //   completed: false
+//   // },
+//   // {
+//   //   task: 'Water Plants',
+//   //   id: 1528817086440,
+//   //   completed: false
+//   // },
+// ];
 
 function App() {
-  const [todo, setTodo] = useState(todoData);
+  const [todo, setTodo] = useState();
+
+  //reducer
+  const [state, dispatch]= useReducer(todoReducer, initialState);
 
 //  const addItem = taskName => {
 //     setTodo([...todo, {...taskName, id:Date.now()}])
-//   }//doesnot
+//   }//does not work
 
 const addItem = taskName => {
   const newTask = {
@@ -62,10 +68,10 @@ const addItem = taskName => {
     );
   }
 
-  // const clearCompleted = () => {
-  //   setTodo(todo.filter(task => !task.completed)
-  //   )
-  // }
+  const clearCompleted = () => {
+    setTodo(todo.filter(task => !task.completed)
+    )
+  }
 
   return (
     <div className="App">
@@ -79,9 +85,9 @@ const addItem = taskName => {
       <TodoList
           todoItem={todo}
           toggleTask={toggleTask}
-          // clearCompleted={this.clearCompleted}
+          clearCompleted={clearCompleted}
         />
-        {/* <button className="clear-btn" onClick={[]}>Clear Completed</button> */}
+        <button className="clear-btn" onClick={clearCompleted}>Clear Completed</button>
     </div>
   );
 }
--- reducer-todo/src/reducers/todoReducer.js
+++ reducer-todo/src/reducers/todoReducer.js
@@ -1,11 +1,24 @@
 // Export Two Things - initialState, reducer fn
 
-export const initialState = {
+export const initialState = [
+    {
     item: 'Learn about reducers',
     completed: false,
     id: 3892987589
-};
+}
+]
 
 export const todoReducer = (state, action) => {
-    return state;
+    console.log(state, action);
+    switch (action.type) {
+        case 'ADD_ITEM':
+            return {
+                ...state,
+                item: action.item,
+                id: Date.now(),
+                completed: false
+            };
+        default:
+            return state;
+    }
 };
